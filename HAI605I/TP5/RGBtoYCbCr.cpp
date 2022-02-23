3cabbf5b613e1b76e0ebfeb2139ddcd387a8b781
Adam S <adam.said@etu.umontpellier.fr>
2022-02-23 18:10:17 +0100

    Ajout TP4 et modif TP3 Coq

--- HAI605I/TP5/RGBtoYCbCr.cpp
+++ HAI605I/TP5/RGBtoYCbCr.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {
   char cNomImgLue[250], cNomImgRed[250], cNomImgGreen[250], cNomImgBlue[250];
-  int nH, nW, nTaille;
+  int nH, nW, nTaille, nTailleY;
 
   if (argc != 5) 
     {
@@ -29,26 +29,27 @@ int main(int argc, char* argv[])
 
   lire_nb_lignes_colonnes_image_ppm(cNomImgLue, &nH, &nW);
   nTaille = (nH * nW) *3;
+  nTailleY = nH * nW;
   printf("Nom image lue : %s\n",cNomImgLue);
   printf("Largeur : %i, Hauteur %i\n", nW, nH);
 
   allocation_tableau(ImgIn, OCTET, nTaille);
-  allocation_tableau(ImgOutY, OCTET, nTaille);
-  allocation_tableau(ImgOutCb, OCTET, nTaille);
-  allocation_tableau(ImgOutCr, OCTET, nTaille);
+  allocation_tableau(ImgOutY, OCTET, nTailleY);
+  allocation_tableau(ImgOutCb, OCTET, nTailleY);
+  allocation_tableau(ImgOutCr, OCTET, nTailleY);
 
   lire_image_ppm(cNomImgLue, ImgIn, nH * nW);
     for (int i=0; i < nH; i++) {
       for (int j=0; j < nW; j++) {
-          ImgOutY[(i*nW+j)] = (0.3*ImgIn[(i*nW+j)*3] + 0.6*ImgIn[(i*nW+j)*3+1] + 0.1*ImgIn[(i*nW+j)*3+2]);
-          ImgOutCb[(i*nW+j)] = -0.1687 * ImgIn[(i*nW+j)*3] -0.3313 * ImgIn[(i*nW+j)*3+1] + 0.5 * ImgIn[(i*nW+j)*3+2] + 128;
-          ImgOutCr[(i*nW+j)] = 0.5 * ImgIn[(i*nW+j)*3] -0.4187 * ImgIn[(i*nW+j)*3+1] -0.0813 * ImgIn[(i*nW+j)*3+2] + 128;
+          ImgOutY[(i*nW+j)] = (0.299 * ImgIn[(i*nW+j)*3] + 0.587 * ImgIn[(i*nW+j)*3+1] + 0.114 * ImgIn[(i*nW+j)*3+2]);
+          ImgOutCb[(i*nW+j)] = ((ImgIn[(i*nW+j)*3+2] - ImgOutY[(i*nW+j)]) * 0.564 + 128);
+          ImgOutCr[(i*nW+j)] = ((ImgIn[(i*nW+j)*3] - ImgOutY[(i*nW+j)]) * 0.713 + 128);
 
       }
     }
-    ecrire_image_pgm(cNomImgRed, ImgOutCr,  nH, nW);
-    ecrire_image_pgm(cNomImgBlue, ImgOutCb,  nH, nW);
-    ecrire_image_pgm(cNomImgGreen, ImgOutY,  nH, nW);
+    ecrire_image_ppm(cNomImgRed, ImgOutCr,  nH, nW);
+    ecrire_image_ppm(cNomImgBlue, ImgOutCb,  nH, nW);
+    ecrire_image_ppm(cNomImgGreen, ImgOutY,  nH, nW);
     free(ImgIn);
     free(ImgOutY);
     free(ImgOutCb);
